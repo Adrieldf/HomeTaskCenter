b5f04b9997004f4b92f812175f92a53369a6711f
Emerson Adierson Jahn <emersonajahn@gmail.com>
2020-10-11 17:53:08 -0300

    Update Login.java

--- src/view/Login.java
+++ src/view/Login.java
@@ -14,16 +14,16 @@ public class Login extends JPanel {
 	private JTextField tfPassword;
 	public Login() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 30, 100, 100, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[]{0, 0, 30, 100, 100, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
 		JLabel lbTitle = new JLabel("Login");
 		lbTitle.setFont(new Font("Tahoma", Font.BOLD, 17));
 		GridBagConstraints gbc_lbTitle = new GridBagConstraints();
-		gbc_lbTitle.gridwidth = 12;
+		gbc_lbTitle.gridwidth = 6;
 		gbc_lbTitle.insets = new Insets(0, 0, 5, 0);
 		gbc_lbTitle.gridx = 0;
 		gbc_lbTitle.gridy = 0;
@@ -32,9 +32,8 @@ public class Login extends JPanel {
 		JLabel lbName = new JLabel("Nome");
 		lbName.setFont(new Font("Tahoma", Font.BOLD, 11));
 		GridBagConstraints gbc_lbName = new GridBagConstraints();
-		gbc_lbName.gridwidth = 3;
 		gbc_lbName.insets = new Insets(0, 0, 5, 5);
-		gbc_lbName.gridx = 6;
+		gbc_lbName.gridx = 1;
 		gbc_lbName.gridy = 2;
 		add(lbName, gbc_lbName);
 		
@@ -43,7 +42,7 @@ public class Login extends JPanel {
 		gbc_tfName.gridwidth = 2;
 		gbc_tfName.insets = new Insets(0, 0, 5, 5);
 		gbc_tfName.fill = GridBagConstraints.HORIZONTAL;
-		gbc_tfName.gridx = 9;
+		gbc_tfName.gridx = 3;
 		gbc_tfName.gridy = 2;
 		add(tfName, gbc_tfName);
 		tfName.setColumns(10);
@@ -51,9 +50,8 @@ public class Login extends JPanel {
 		JLabel lbPassword = new JLabel("Senha");
 		lbPassword.setFont(new Font("Tahoma", Font.BOLD, 11));
 		GridBagConstraints gbc_lbPassword = new GridBagConstraints();
-		gbc_lbPassword.gridwidth = 3;
 		gbc_lbPassword.insets = new Insets(0, 0, 5, 5);
-		gbc_lbPassword.gridx = 6;
+		gbc_lbPassword.gridx = 1;
 		gbc_lbPassword.gridy = 4;
 		add(lbPassword, gbc_lbPassword);
 		
@@ -63,7 +61,7 @@ public class Login extends JPanel {
 		gbc_tfPassword.gridwidth = 2;
 		gbc_tfPassword.insets = new Insets(0, 0, 5, 5);
 		gbc_tfPassword.fill = GridBagConstraints.HORIZONTAL;
-		gbc_tfPassword.gridx = 9;
+		gbc_tfPassword.gridx = 3;
 		gbc_tfPassword.gridy = 4;
 		add(tfPassword, gbc_tfPassword);
 		tfPassword.setColumns(10);
@@ -72,7 +70,7 @@ public class Login extends JPanel {
 		GridBagConstraints gbc_btnLogin = new GridBagConstraints();
 		gbc_btnLogin.gridwidth = 2;
 		gbc_btnLogin.insets = new Insets(0, 0, 5, 5);
-		gbc_btnLogin.gridx = 8;
+		gbc_btnLogin.gridx = 2;
 		gbc_btnLogin.gridy = 6;
 		add(btnLogin, gbc_btnLogin);
 		
@@ -80,7 +78,7 @@ public class Login extends JPanel {
 		GridBagConstraints gbc_btnCancel = new GridBagConstraints();
 		gbc_btnCancel.anchor = GridBagConstraints.EAST;
 		gbc_btnCancel.insets = new Insets(0, 0, 5, 5);
-		gbc_btnCancel.gridx = 10;
+		gbc_btnCancel.gridx = 4;
 		gbc_btnCancel.gridy = 6;
 		add(btnCancel, gbc_btnCancel);
 	}
